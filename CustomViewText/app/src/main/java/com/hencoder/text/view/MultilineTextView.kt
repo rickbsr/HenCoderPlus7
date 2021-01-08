81bc97eae4c44c06693cbf03fc9079256aeae647
Rick Ren <rick_ren@askey.com>
2021-01-08 16:46:13 +0800

    自定义 View 绘制：文字的测量

--- CustomViewText/app/src/main/java/com/hencoder/text/view/MultilineTextView.kt
+++ CustomViewText/app/src/main/java/com/hencoder/text/view/MultilineTextView.kt
@@ -17,47 +17,99 @@ private val IMAGE_SIZE = 150.dp
 private val IMAGE_PADDING = 50.dp
 
 class MultilineTextView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
-  val text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tristique urna tincidunt maximus viverra. Maecenas commodo pellentesque dolor ultrices porttitor. Vestibulum in arcu rhoncus, maximus ligula vel, consequat sem. Maecenas a quam libero. Praesent hendrerit ex lacus, ac feugiat nibh interdum et. Vestibulum in gravida neque. Morbi maximus scelerisque odio, vel pellentesque purus ultrices quis. Praesent eu turpis et metus venenatis maximus blandit sed magna. Sed imperdiet est semper urna laoreet congue. Praesent mattis magna sed est accumsan posuere. Morbi lobortis fermentum fringilla. Fusce sed ex tempus, venenatis odio ac, tempor metus."
-  private val textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
-    textSize = 16.dp
-  }
-  private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-    textSize = 16.dp
-  }
-  private val bitmap = getAvatar(IMAGE_SIZE.toInt())
-  private val fontMetrics = Paint.FontMetrics()
 
-  override fun onDraw(canvas: Canvas) {
-//    val staticLayout = StaticLayout(text, textPaint, width, Layout.Alignment.ALIGN_NORMAL, 1f, 0f, false)
-//    staticLayout.draw(canvas)
-    canvas.drawBitmap(bitmap, width - IMAGE_SIZE, IMAGE_PADDING, paint)
-    paint.getFontMetrics(fontMetrics)
-    val measuredWidth = floatArrayOf(0f)
-    var start = 0
-    var count: Int
-    var verticalOffset = - fontMetrics.top
-    var maxWidth: Float
-    while (start < text.length) {
-      maxWidth = if (verticalOffset + fontMetrics.bottom < IMAGE_PADDING
-        || verticalOffset + fontMetrics.top > IMAGE_PADDING + IMAGE_SIZE) {
-        width.toFloat()
-      } else {
-        width.toFloat() - IMAGE_SIZE
-      }
-      count = paint.breakText(text, start, text.length, true, maxWidth, measuredWidth)
-      canvas.drawText(text, start, start + count, 0f, verticalOffset, paint)
-      start += count
-      verticalOffset += paint.fontSpacing
+    // Lorem Ipsum：沒意義的文字，方便測試使用。
+    val text =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tristique urna tincidunt maximus viverra. Maecenas commodo pellentesque dolor ultrices porttitor. Vestibulum in arcu rhoncus, maximus ligula vel, consequat sem. Maecenas a quam libero. Praesent hendrerit ex lacus, ac feugiat nibh interdum et. Vestibulum in gravida neque. Morbi maximus scelerisque odio, vel pellentesque purus ultrices quis. Praesent eu turpis et metus venenatis maximus blandit sed magna. Sed imperdiet est semper urna laoreet congue. Praesent mattis magna sed est accumsan posuere. Morbi lobortis fermentum fringilla. Fusce sed ex tempus, venenatis odio ac, tempor metus."
+
+    private val textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = 16.dp
     }
-  }
 
-  fun getAvatar(width: Int): Bitmap {
-    val options = BitmapFactory.Options()
-    options.inJustDecodeBounds = true
-    BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
-    options.inJustDecodeBounds = false
-    options.inDensity = options.outWidth
-    options.inTargetDensity = width
-    return BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
-  }
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = 16.dp
+    }
+
+    private val bitmap = getAvatar(IMAGE_SIZE.toInt())
+    private val fontMetrics = Paint.FontMetrics()
+
+    override fun onDraw(canvas: Canvas) {
+        // 在新版本已經改用 Builder 方式。
+//        val staticLayout = StaticLayout(
+//            text,
+//            textPaint,
+//            width, // 寬度
+//            Layout.Alignment.ALIGN_NORMAL, // 對齊方式
+//            1f, // 文字間距調整（乘除），為原始間距的倍數，1 代表不改變。
+//            0f, // 文字間距調整（加減）。
+//            false // 行與行之間是否需要額外間距。
+//        )
+
+        // 以 Builder 方式。
+//        val staticLayout = StaticLayout.Builder
+//            .obtain(text, 0, text.length, textPaint, width)
+//            .setAlignment(Layout.Alignment.ALIGN_NORMAL)
+//            .build()
+
+//        staticLayout.draw(canvas)
+
+        canvas.drawBitmap(bitmap, width - IMAGE_SIZE, IMAGE_PADDING, paint)
+        paint.getFontMetrics(fontMetrics)
+
+        /*
+        val measuredWidth = floatArrayOf(0f)
+
+        // 第一行
+        var count = paint.breakText( // breakText() 可以判斷能寫多少文字，其返回值為到哪個文字。
+            text,
+            true, // 測量文字的方向，順向：true，逆向：false。
+            width.toFloat(), // 要撰寫的寬度。
+            measuredWidth // 測量寬度。
+        )
+        canvas.drawText(text, 0, count, 0f, -fontMetrics.top, paint)
+
+        // 第二行
+        val oldCount = count
+        count = paint.breakText(text, count, text.length, true, width.toFloat(), measuredWidth)
+        canvas.drawText(
+            text, oldCount, oldCount + count, 0f,
+            -fontMetrics.top + paint.fontSpacing, // 折行的方式可以用 * 2，但更建議的方式是 paint.fontSpacing 。
+            paint
+        )
+         */
+        val measuredWidth = floatArrayOf(0f)
+
+        var start = 0
+        var count: Int
+        var verticalOffset = -fontMetrics.top // 初始偏移
+        var maxWidth: Float // 可繪製文字的最大寬度
+
+        while (start < text.length) { // 至文字繪製結束。
+
+            // 判斷可繪製的寬度
+            maxWidth = if (verticalOffset + fontMetrics.bottom < IMAGE_PADDING || // 頂部
+                verticalOffset + fontMetrics.top > IMAGE_PADDING + IMAGE_SIZE // 底部
+            ) {
+                width.toFloat()
+            } else {
+                width.toFloat() - IMAGE_SIZE
+            }
+
+            count = paint.breakText(text, start, text.length, true, maxWidth, measuredWidth)
+            canvas.drawText(text, start, start + count, 0f, verticalOffset, paint)
+
+            start += count // 刷新文字起始點
+            verticalOffset += paint.fontSpacing // 換行。
+        }
+    }
+
+    private fun getAvatar(width: Int): Bitmap {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
+        options.inJustDecodeBounds = false
+        options.inDensity = options.outWidth
+        options.inTargetDensity = width
+        return BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
+    }
 }
--- CustomViewText/app/src/main/java/com/hencoder/text/view/SportView.kt
+++ CustomViewText/app/src/main/java/com/hencoder/text/view/SportView.kt
@@ -17,45 +17,97 @@ private val RING_WIDTH = 20.dp
 private val RADIUS = 150.dp
 
 class SportView(context: Context, attrs: AttributeSet?) :
-  View(context, attrs) {
-  private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-    textSize = 100.dp
-    typeface = ResourcesCompat.getFont(context, R.font.font)
-    textAlign = Paint.Align.CENTER
-  }
-  private val bounds = Rect()
-  private val fontMetrics = Paint.FontMetrics()
+    View(context, attrs) {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        /*
+         * Font Size：
+         * - sp：文字專用單位，其會隨著使用者手機系統的設定而改變大小。
+         * - dp：與像素、密度有關的單位，由於繪製文字的特性偏向圖片，為避免跑板，因此一般繪製文字建議使用 dp。
+         */
+        textSize = 100.dp
 
-  override fun onDraw(canvas: Canvas) {
-    super.onDraw(canvas)
+        /*
+         * Font Type：
+         * - typeface：文字種類，例如微軟正黑體、標楷體…等。
+         * - font 子集：如 ubuntu 有 ubuntu-medium、ubuntu-bold…等。
+         * - isFakeBoldText：使用非原本粗體，而是使用 Android 本身的演算法加粗文字。
+         */
+        typeface = ResourcesCompat.getFont(context, R.font.font)
+//        isFakeBoldText = true
 
-    // 绘制环
-    paint.style = Paint.Style.STROKE
-    paint.color = CIRCLE_COLOR
-    paint.strokeWidth = RING_WIDTH
-    canvas.drawCircle(width / 2f, height / 2f, RADIUS, paint)
+        // 橫向居中對齊
+        textAlign = Paint.Align.CENTER
+    }
+    private val bounds = Rect() // 僅有 top、bottom
+    private val fontMetrics = Paint.FontMetrics()// 有 top、ascend、descend、bottom
 
-    // 绘制进度条
-    paint.color = HIGHLIGHT_COLOR
-    paint.strokeCap = Paint.Cap.ROUND
-    canvas.drawArc(width / 2f - RADIUS, height / 2f - RADIUS, width / 2f + RADIUS, height / 2f + RADIUS, -90f, 225f, false, paint)
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
 
-    // 绘制文字
-    paint.textSize = 100.dp
-    paint.style = Paint.Style.FILL
-    paint.getFontMetrics(fontMetrics)
-    canvas.drawText("aaaa", width / 2f, height / 2f - (fontMetrics.ascent + fontMetrics.descent) / 2f, paint)
+        // 绘制环
+        paint.style = Paint.Style.STROKE // 中空模式
+        paint.color = CIRCLE_COLOR
+        paint.strokeWidth = RING_WIDTH
+        canvas.drawCircle(width / 2f, height / 2f, RADIUS, paint)
 
-    // 绘制文字2
-    paint.textSize = 150.dp
-    paint.textAlign = Paint.Align.LEFT
-    paint.getFontMetrics(fontMetrics)
-    paint.getTextBounds("abab", 0, "abab".length, bounds)
-    canvas.drawText("abab", - bounds.left.toFloat(), - bounds.top.toFloat(), paint)
+        // 绘制进度条
+        paint.color = HIGHLIGHT_COLOR
+        paint.strokeCap = Paint.Cap.ROUND // 線條兩端頭的樣式
+        canvas.drawArc(
+            width / 2f - RADIUS,
+            height / 2f - RADIUS,
+            width / 2f + RADIUS,
+            height / 2f + RADIUS,
+            -90f,
+            225f,
+            false,
+            paint
+        )
 
-    // 绘制文字3
-    paint.textSize = 15.dp
-    paint.getTextBounds("abab", 0, "abab".length, bounds)
-    canvas.drawText("abab", - bounds.left.toFloat(), - bounds.top.toFloat(), paint)
-  }
+        // 上下置中：以基準線為底（預設）
+//        paint.style = Paint.Style.FILL
+//        canvas.drawText("abab", width / 2f, height / 2f, paint)
+
+        // 上下置中：以文字邊界並扣除偏移量（完全置中），但由於文字邊界會因為文字不同有高低差，因此視覺上會有上下漂移的感覺；不適合動態問字。
+//        paint.style = Paint.Style.FILL
+//        // 藉由 getTextBounds 來獲得文字邊界，須傳入 Rect()。
+//        paint.getTextBounds("abab", 0, "abab".length, bounds)
+//        // 高度減去高度偏移量：(bounds.top + bounds.bottom) / 2
+//        canvas.drawText(
+//            "abab", width / 2f, height / 2f - (bounds.top + bounds.bottom) / 2, paint
+//        )
+
+        // 上下置中：以文字區間邊界並扣除偏移量（相對置中），與文字無關，因此不會因為文字不同而有差異，適合動態文字。
+//        paint.style = Paint.Style.FILL // 填充模式
+//        // 藉由 fontMetrics 來獲取文字區間。
+//        paint.getFontMetrics(fontMetrics)
+//        canvas.drawText(
+//            "aaaa",
+//            width / 2f,
+//            height / 2f - (fontMetrics.ascent + fontMetrics.descent) / 2f,
+//            paint
+//        )
+
+        // 向上對齊、貼左
+        paint.textSize = 150.dp
+        paint.style = Paint.Style.FILL
+        paint.textAlign = Paint.Align.LEFT
+        paint.getFontMetrics(fontMetrics)
+//        paint.getTextBounds( // 獲取文字邊界
+//            "abab",
+//            0,
+//            "abab".length,
+//            bounds // 傳入 Rect() 用於存放結果
+//        )
+        // 建議用 top，用 ascent 在某些文字時可能被切邊。
+        canvas.drawText("abab", 0f, -fontMetrics.top, paint)
+//        canvas.drawText("abab", 0f, -bounds.top.toFloat(), paint)
+
+
+        // 處理文字與邊界的間隙。
+        paint.textSize = 15.dp
+        paint.getTextBounds("abab", 0, "abab".length, bounds)
+        // 若到底還有一點點縫隙，其實那是屬於文字的自然邊界，也就是說該縫隙在系統的判定中屬於文字，因此，我們無法解決。
+        canvas.drawText("abab", -bounds.left.toFloat(), -bounds.top.toFloat(), paint)
+    }
 }
