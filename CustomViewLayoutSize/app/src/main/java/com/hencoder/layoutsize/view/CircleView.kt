4af446c77a620202f3970f17ef467f5060302a5f
Rick <rick8220602@gmail.com>
2021-01-17 15:04:46 +0800

    自定义 View 布局：自定义布局之尺寸的自定义

--- CustomViewLayoutSize/app/src/main/java/com/hencoder/layoutsize/view/CircleView.kt
+++ CustomViewLayoutSize/app/src/main/java/com/hencoder/layoutsize/view/CircleView.kt
@@ -7,22 +7,61 @@ import android.util.AttributeSet
 import android.view.View
 import com.hencoder.layoutsize.dp
 
+/**
+ * 完全自定義 View 尺寸
+ */
 private val RADIUS = 100.dp
 private val PADDING = 100.dp
 
 class CircleView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
-  private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-  override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-    val size = ((PADDING + RADIUS) * 2).toInt()
-    val width = resolveSize(size, widthMeasureSpec)
-    val height = resolveSize(size, heightMeasureSpec)
-    setMeasuredDimension(width, height)
-  }
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val size = ((PADDING + RADIUS) * 2).toInt()
 
-  override fun onDraw(canvas: Canvas) {
-    super.onDraw(canvas)
+        /*
+         * 需以父 View 的要求作為限制：
+         *
+         * - 父類的限制會以參數的方式傳入，如 widthMeasureSpec 與 heightMeasureSpec；其包含資訊有限制模式、
+         *   大小與精確值。
+         * - 限制的模式主要有三類：
+         *   1. 精準的限制
+         *   2. 範圍區間
+         *   3. 不限制
+         */
+//        val specWidthMode = MeasureSpec.getMode(widthMeasureSpec) // 取得限制模式
+//        val specWidthSize = MeasureSpec.getSize(widthMeasureSpec)
+//        val width = when (specWidthMode) {
+//            MeasureSpec.EXACTLY -> specWidthSize // 精確值，即測量值沒用，用 spec 值
+//            MeasureSpec.AT_MOST -> // 範圍區間
+//                if (size > specWidthSize) specWidthSize else size
+//            else -> size // MeasureSpec.UNSPECIFIED, 不限制。
+//        }
 
-    canvas.drawCircle(PADDING + RADIUS, PADDING + RADIUS, RADIUS, paint)
-  }
+        /*
+         * resolveSize()：
+         *
+         * 上述的流程方法就是 resolveSize() 的實作。
+         */
+        val width = resolveSize(size, widthMeasureSpec)
+        val height = resolveSize(size, heightMeasureSpec)
+
+        /*
+         * resolveSizeAndState()：
+         *
+         * 比起 resolveSize()，在 MeasureSpec.AT_MOST 模式且 specSize 小於 size 的情況下，
+         * 其會給予 MEASURED_STATE_TOO_SMALL 標籤，它就可以向父 View 要求重新測量。
+         *
+         * 結論：理論上，resolveSizeAndState() 是一個理想的方式，但實際上，大多數的父 View 都有沒實作該功能，
+         * 也就是說，他們並不會去讀取 MEASURED_STATE_TOO_SMALL 標籤，因此 resolveSize() 是一個比較建議的用法。
+         */
+//        val width = resolveSizeAndState()
+
+        setMeasuredDimension(width, height)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawCircle(PADDING + RADIUS, PADDING + RADIUS, RADIUS, paint)
+    }
 }
--- CustomViewLayoutSize/app/src/main/java/com/hencoder/layoutsize/view/SquareImageView.kt
+++ CustomViewLayoutSize/app/src/main/java/com/hencoder/layoutsize/view/SquareImageView.kt
@@ -5,12 +5,36 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import kotlin.math.min
 
-class SquareImageView(context: Context?, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
-  override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+/**
+ * 簡單改寫已有的 View 尺寸
+ */
+class SquareImageView(context: Context?, attrs: AttributeSet?) :
+    AppCompatImageView(context, attrs) {
 
-    val size = min(measuredWidth, measuredHeight)
+    /*
+     * 在 Layout 中強行修改子 View 的尺寸：
+     *
+     * 雖然有效，但應避免這類強制衝突的修改方式；但此處的操作會使父 View 無法得知子 View 的實際佈局邊界，
+     * 而當 View 父無法掌控全局的佈局時，就可能會出現非預期的情況。
+     *
+     * 結論：勁量避免覆寫 layout()。
+     */
+//    override fun layout(l: Int, t: Int, r: Int, b: Int) {
+//        val width = r - l
+//        val height = b - t
+//        val size = min(width, height)
+//        super.layout(l, t, l + size, t + size)
+//    }
 
-    setMeasuredDimension(size, size)
-  }
+    /*
+     * 自定義 View 尺寸。
+     *
+     * - Step1. 藉由 getMeasuredWidth() 和 getMeasuredHeight() 來獲取量測到的尺寸。
+     * - Step2. 藉由 setMeasuredDimension() 來設置自定義尺寸。
+     */
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val size = min(measuredWidth, measuredHeight)
+        setMeasuredDimension(size, size)
+    }
 }
